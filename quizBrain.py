e6d4746b1b928380025b788eb2be199be1e97d60
Atishay Jain <61509695+atishay2305-hub@users.noreply.github.com>
2023-11-16 18:51:04 -0500

    Update quizBrain.py

--- quizBrain.py
+++ quizBrain.py
@@ -1,39 +1,16 @@
-# quiz_server.py
-from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
+# quiz_brain.py
 
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/quiz_db"  # Change the URI accordingly
-mongo = PyMongo(app)
+class QuizBrain:
+    def __init__(self):
+        self.questions = []
+        self.current_question_index = 0
 
-from quiz_brain import QuizBrain
+    def add_question(self, question, correct_answer):
+        self.questions.append({"question": question, "correct_answer": correct_answer})
 
-quiz_brain = QuizBrain()
-
-# Add some sample questions
-quiz_brain.add_question("What is the capital of France?", "Paris")
-quiz_brain.add_question("What is 2 + 2?", "4")
-# Add more questions as needed
-
-@app.route('/quiz', methods=['GET', 'POST'])
-def quiz():
-    if request.method == 'GET':
-        question_data = quiz_brain.get_next_question()
-        if question_data:
-            return jsonify(question_data)
-        else:
-            return jsonify({"message": "Quiz completed!"})
-
-    elif request.method == 'POST':
-        data = request.json
-        user_data = {
-            "name": data["name"],
-            "marks": data["marks"],
-            "quiz_attempted": data["quiz_attempted"]
-        }
-        mongo.db.users.insert_one(user_data)
-        return jsonify({"message": "User data stored successfully!"})
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    def get_next_question(self):
+        if self.current_question_index < len(self.questions):
+            question_data = self.questions[self.current_question_index]
+            self.current_question_index += 1
+            return question_data
+        return None
