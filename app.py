75cb6dcf90a07802a1e6cde950f80a15b116d7df
Atishay Jain <ajain70@stevens.edu>
2023-11-17 10:13:55 -0500

    Initial Setup with Flask and MongoDB

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-# app.py
-from quiz_server import app
+from flask import Flask
+from flask_pymongo import PyMongo
+from views import views
+
+app = Flask(__name__)
+
+app.config['MONGO_URI'] = 'mongodb://myuser:mypassword@localhost:8000/SSW-500-project'
+
+mongo = PyMongo(app)
+
+app.register_blueprint(views, url_prefix="/")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
